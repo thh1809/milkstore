d00bc72aa230272e2650f9b486f01f490491c5bd
hai hong <133628015+thh1809@users.noreply.github.com>
2025-07-24 11:12:20 +0700

    Your commit message

--- app/src/main/java/com/milkstoremobile_fronend/api/ApiClient.java
+++ app/src/main/java/com/milkstoremobile_fronend/api/ApiClient.java
@@ -3,7 +3,9 @@ package com.milkstoremobile_fronend.api;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.converter.scalars.ScalarsConverterFactory;
+
 import com.milkstoremobile_fronend.api.services.MessageApiService;
+import com.milkstoremobile_fronend.api.services.PaymentApiService;
 
 
 public class ApiClient {
@@ -28,49 +30,11 @@ public class ApiClient {
     public static MessageApiService getMessageApiService() {
         return getClient().create(MessageApiService.class);
     }
-}
 
-//public class ApiClient {
-//    private static Retrofit retrofit = null;
-//
-//    public static Retrofit getClient() {
-//        if (retrofit == null) {
-//            retrofit = new Retrofit.Builder()
-//                    .baseUrl("http://10.0.2.2:8081/") // Nếu chạy trên emulator
-//                    .addConverterFactory(ScalarsConverterFactory.create())
-//                    .addConverterFactory(GsonConverterFactory.create())
-//                    .addConverterFactory(g)
-//                    .build();
-//        }
-//        return retrofit;
-//    }
-//
-//    public static MessageApiService getMessageApiService() {
-//        return getClient().create(MessageApiService.class);
-//    }
-//}
-//public class ApiClient {
-//    private static Retrofit retrofit;
-//    // Đúng: base URL phải kết thúc bằng dấu "/"
-//    private static final String BASE_URL = "http://10.0.2.2:8081/api/";
-//
-//    // Hàm này trả về instance Retrofit dùng chung cho mọi service
-//    public static Retrofit getClient() {
-//        if (retrofit == null) {
-//            retrofit = new Retrofit.Builder()
-//                    .baseUrl(BASE_URL)
-//                    .addConverterFactory(ScalarsConverterFactory.create())
-//                    .addConverterFactory(GsonConverterFactory.create())
-//                    .build();
-//        }
-//        return retrofit;
-//    }
-//
-//    // Service cho Message
-//    public static MessageApiService getMessageApiService() {
-//        return getClient().create(MessageApiService.class);
-//    }
-//
-//    // Nếu có các service khác, bạn cũng tạo hàm tương tự:
-//    // public static ProductService getProductService() { ... }
-//}
+    // ✅ Service cho Payment
+    public static PaymentApiService getPaymentApiService() {
+        return getClient().create(PaymentApiService.class);
+    }
+
+
+}
